2ca57e7c5bde6e41af13cda1ca5f2e9d0c6f97a9
munikumar2003 <munikumarchemuru2003@gmail.com>
2025-11-05 18:50:27 +0530

    added new branches to the advanced exam

--- src/pages/CollegeFinder.tsx
+++ src/pages/CollegeFinder.tsx
@@ -108,7 +108,7 @@ export default function CollegeFinder() {
 
   const branches = {
     jee_main: ['Computer Science Engineering','Artificial Engineering','Data Science and Engineering','Information Technology', 'Mechanical Engineering', 'Electrical Engineering', 'Civil Engineering', 'Electronics Engineering','Chemical Engineering', 'Aerospace Engineering', 'Biotechnology','Textile Technology','Instrumentation and Control Engineering', 'Mathematics and Computing','Industrial and Production Engineering','Mining Engineering','Metallurgy and Materials Engineering'],
-    jee_advanced: ['Computer Science Engineering', 'Mechanical Engineering', 'Electrical Engineering', 'Civil Engineering', 'Electronics Engineering', 'Chemical Engineering', 'Aerospace Engineering', 'Metallurgical Engineering','Environmental Science and Engineering','Industrial Engineering'],
+    jee_advanced: ['Computer Science Engineering','Data Science and Artificial Engineering', 'Mechanical Engineering', 'Electrical Engineering', 'Civil Engineering', 'Electronics Engineering', 'Chemical Engineering', 'Aerospace Engineering', 'Metallurgical Engineering','Environmental Science and Engineering','Industrial Engineering'],
     neet: ['MBBS', 'BDS', 'BAMS', 'BHMS', 'BUMS', 'Veterinary Science', 'B.Sc Nursing', 'Physiotherapy'],
     gate: ['Computer Science', 'Mechanical Engineering', 'Electrical Engineering', 'Civil Engineering', 'Electronics Engineering', 'Chemical Engineering', 'Aerospace Engineering', 'Environmental Engineering'],
     cat: ['MBA', 'PGDM', 'Executive MBA', 'MBA in Finance', 'MBA in Marketing', 'MBA in HR', 'MBA in Operations', 'MBA in International Business']
